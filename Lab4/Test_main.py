8fbfe76872ccb79c208e9c7e5749781e5d1bffa1
AntonBessarab <antonbessarab55@gmail.com>
2024-10-19 17:38:09 +0300

    Lab4 jenkins

---
+++ Lab4/Test_main.py
@@ -0,0 +1,60 @@
+import unittest
+from main import add_product_to_queue, get_first_product_from_queue, watch_all_orders_in_queue, clear_orders
+
+
+class TestOrderQueue(unittest.TestCase):
+
+    def setUp(self) -> None:
+        clear_orders()
+
+    def test_add_product_to_queue(self):
+        order = add_product_to_queue("Apple", 5)
+        self.assertEqual(len(watch_all_orders_in_queue()), 1)
+        self.assertEqual(order["product_name"], "Apple")
+        self.assertEqual(order["quantity"], 5)
+
+    # Перевірка виключення
+    def test_add_product_to_queue_invalid_quantity(self):
+        with self.assertRaises(ValueError):
+            add_product_to_queue("Banana", -6)
+
+    def test_get_first_product_from_queue(self):
+        add_product_to_queue("Orange", 3)
+        add_product_to_queue("Lemon", 7)
+        order = get_first_product_from_queue()
+        self.assertEqual(order["product_name"], "Orange")
+        order = get_first_product_from_queue()
+        self.assertEqual(order["product_name"], "Lemon")
+        self.assertEqual(len(watch_all_orders_in_queue()), 0)
+
+    def test_get_first_product_from_queue_empty(self):
+        order = get_first_product_from_queue()
+        self.assertEqual(order, {})
+
+    # Перевірка негативного сценарію
+    @unittest.expectedFailure
+    def test_expected_failure_get_from_empty_queue(self):
+        with self.assertRaises(ValueError):
+            get_first_product_from_queue()
+
+    def test_get_all_orders_in_queue(self):
+        add_product_to_queue("Samsung", 5)
+        add_product_to_queue("Casio", 10)
+        orders = watch_all_orders_in_queue()
+
+        expected_orders = [
+            {"product_name": "Samsung", "quantity": 5},
+            {"product_name": "Casio", "quantity": 10}
+        ]
+
+        self.assertEqual(orders, expected_orders)
+
+    def test_clear_orders(self):
+        add_product_to_queue("Apple", 5)
+        result = clear_orders()
+        self.assertTrue(result)
+        self.assertEqual(len(watch_all_orders_in_queue()), 0)
+
+
+#if __name__ == '__main__':
+    #unittest.main()
---
+++ Lab4/main.py
@@ -0,0 +1,70 @@
+import queue
+
+order_queue = queue.Queue()
+
+
+def add_product_to_queue(name, quantity) -> dict:
+    if quantity <= 0:
+        raise ValueError("Quantity must be greater than 0")
+
+    order = {
+        "product_name": name,
+        "quantity": quantity
+    }
+    order_queue.put(order)
+    print(f"Order: {order}, added to queue")
+    return order
+
+
+def get_first_product_from_queue() -> dict:
+    if not order_queue.empty():
+        order = order_queue.get()
+        print(f"Get product name: {order["product_name"]}")
+        return order
+    else:
+        print("Queue empty")
+        return {}
+
+
+def watch_all_orders_in_queue() -> list:
+    orders = list(order_queue.queue)
+    print(f"Current orders in queue: {orders}")
+    return orders
+
+
+def clear_orders() -> bool:
+    with order_queue.mutex:
+        order_queue.queue.clear()
+    print("Now queue is cleared")
+    return True
+
+
+def menu() -> None:
+    while True:
+        print("\n=== Order Management Menu ===")
+        print("1. Add product to queue")
+        print("2. Get first product from queue")
+        print("3. See all order")
+        print("4. Clear orders queue")
+        print("5. Exit")
+
+        choice = input("Choose an option (1-5): ")
+
+        if choice == "1":
+            product_name = input("Enter product name: ")
+            quantity = int(input("Enter quantity: "))
+            add_product_to_queue(product_name, quantity)
+        elif choice == "2":
+            get_first_product_from_queue()
+        elif choice == "3":
+            watch_all_orders_in_queue()
+        elif choice == "4":
+            clear_orders()
+        elif choice == "5":
+            print("Exiting the program.")
+            break
+        else:
+            print("Invalid choice, please choose again.")
+
+
+menu()
