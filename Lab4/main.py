90e84fa7e2b7f9f4e89b261b4bbc431334e29363
AntonBessarab <antonbessarab55@gmail.com>
2024-10-27 16:50:58 +0200

    Test for github-webhook

--- Lab4/main.py
+++ Lab4/main.py
@@ -47,6 +47,7 @@ def menu() -> None:
         print("3. See all order")
         print("4. Clear orders queue")
         print("5. Exit")
+        print("Github-hook test")
 
         choice = input("Choose an option (1-5): ")
 
